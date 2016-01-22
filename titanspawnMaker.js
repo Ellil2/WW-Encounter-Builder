620a6b560d16dfd5bbc9be6e34ccdee10863602b
Ellil1 <emile@douilhet.com>
2016-01-22 12:08:35 +0000

    Update titanspawnMaker.js

--- titanspawnMaker.js
+++ titanspawnMaker.js
@@ -5,7 +5,7 @@ function npcChoice(){
 //fightTypeVar = 2
 //fightDifficultyVar = 1
 
-fightCompute = Math.floor((6+playerLevelVar*3)*playerNumberVar*(1.5-(fightDifficultyVar/4)))
+fightCompute = Math.floor((playerLevelVar*3)*playerNumberVar*(1.5-(fightDifficultyVar/4)))
 
 
      function decision(argument,numberOn){ 
